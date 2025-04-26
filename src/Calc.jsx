bdfd8302662f49b0964ca003d168058784b274fc
pbaki <pbaki999@gmail.com>
2025-04-26 16:34:17 +0200

    Prospecting calc

--- src/Calc.jsx
+++ src/Calc.jsx
@@ -2,12 +2,24 @@ import { useState } from "react";
 import "./Calc.css";
 
 function Calc() {
-  const [buttonhandler, setButtonhandler] = useState(true);
+  const [buttonhandler, setButtonhandler] = useState(1);
   const [idust, setIdust] = useState(0);
   const [greatercosmic, setGreatercosmic] = useState(0);
   const [dreamshard, setDreamshard] = useState(0);
   const [eternalEarth, setEternalEarth] = useState(0);
   const [smallGem, setSmallGem] = useState(0);
+
+  const [Saronite, setSaronite] = useState(0);
+  const [Cobalt, setCobalt] = useState(0);
+  const [ShadowCrystal, setShadowCrystal] = useState(0);
+  const [Chalcedony, setChalcedony] = useState(0);
+  const [DarkJade, setDarkJade] = useState(0);
+  const [MonarchTopaz, setMonarchTopaz] = useState(0);
+  const [ForestEmerald, setForestEmerald] = useState(0);
+  const [SkySapphire, setSkySapphire] = useState(0);
+  const [AutumnsGlow, setAutumnsGlow] = useState(0);
+  const [ScarletRuby, setScarletRuby] = useState(0);
+  const [TwilightOpal, setTwilightOpal] = useState(0);
 
   function handleIdust(e) {
     setIdust(parseFloat(e.target.value));
@@ -24,33 +36,90 @@ function Calc() {
   function handleSmallGem(e) {
     setSmallGem(parseFloat(e.target.value));
   }
+  function handleShadowCrystal(e) {
+    setShadowCrystal(parseFloat(e.target.value));
+  }
+  function handleChalcedony(e) {
+    setChalcedony(parseFloat(e.target.value));
+  }
+  function handleDarkJade(e) {
+    setDarkJade(parseFloat(e.target.value));
+  }
+  function handleMonarchTopaz(e) {
+    setMonarchTopaz(parseFloat(e.target.value));
+  }
+  function handleForestEmerald(e) {
+    setForestEmerald(parseFloat(e.target.value));
+  }
+  function handleSkySapphire(e) {
+    setSkySapphire(parseFloat(e.target.value));
+  }
+  function handleAutumnsGlow(e) {
+    setAutumnsGlow(parseFloat(e.target.value));
+  }
+  function handleScarletRuby(e) {
+    setScarletRuby(parseFloat(e.target.value));
+  }
+  function handleTwilightOpal(e) {
+    setTwilightOpal(parseFloat(e.target.value));
+  }
+  function handleCobalt(e) {
+    setCobalt(parseFloat(e.target.value));
+  }
+  function handleSaronite(e) {
+    setSaronite(parseFloat(e.target.value));
+  }
   function handleSmallButton() {
-    setButtonhandler(true);
+    setButtonhandler(1);
   }
   function handleBigButton() {
-    setButtonhandler(false);
+    setButtonhandler(2);
+  }
+  function handleProspectingButton() {
+    setButtonhandler(3);
   }
   return (
     <div className="content">
       <div className="buttons">
         <button onClick={handleSmallButton}>Small</button>
         <button onClick={handleBigButton}>Big</button>
+        <button onClick={handleProspectingButton}>Prospecting</button>
       </div>
       <div className="matsprices">
-        <MatsPrices
-          handleEternalEarth={handleEternalEarth}
-          handleSmallGem={handleSmallGem}
-        />
+        {buttonhandler == 1 || buttonhandler == 2 ? (
+          <MatsPrices
+            handleEternalEarth={handleEternalEarth}
+            handleSmallGem={handleSmallGem}
+            buttonhandler={buttonhandler}
+          />
+        ) : (
+          <Prospecting
+            handleCobalt={handleCobalt}
+            handleSaronite={handleSaronite}
+            handleShadowCrystal={handleShadowCrystal}
+            handleChalcedony={handleChalcedony}
+            handleDarkJade={handleDarkJade}
+            handleMonarchTopaz={handleMonarchTopaz}
+            handleForestEmerald={handleForestEmerald}
+            handleSkySapphire={handleSkySapphire}
+            handleAutumnsGlow={handleAutumnsGlow}
+            handleScarletRuby={handleScarletRuby}
+            handleTwilightOpal={handleTwilightOpal}
+            handleSmallGem={handleSmallGem}
+          />
+        )}
       </div>
-      <div className="enchprices">
-        <EnchPrices
-          handleIdust={handleIdust}
-          handleGreatercosmic={handleGreatercosmic}
-          handleDreamshard={handleDreamshard}
-        />
+      <div className="resultprices">
+        {buttonhandler == 1 || buttonhandler == 2 ? (
+          <EnchPrices
+            handleIdust={handleIdust}
+            handleGreatercosmic={handleGreatercosmic}
+            handleDreamshard={handleDreamshard}
+          />
+        ) : null}
       </div>
       <div className="profit">
-        {buttonhandler == true ? (
+        {buttonhandler == 1 ? (
           <SmallItem
             idust={idust}
             greatercosmic={greatercosmic}
@@ -58,12 +127,27 @@ function Calc() {
             eternalEarth={eternalEarth}
             smallGem={smallGem}
           />
-        ) : (
+        ) : buttonhandler == 2 ? (
           <Bigitem
             idust={idust}
             greatercosmic={greatercosmic}
             dreamshard={dreamshard}
             eternalEarth={eternalEarth}
+          />
+        ) : (
+          <ProspectingResult
+            Cobalt={Cobalt}
+            Saronite={Saronite}
+            smallGem={smallGem}
+            ShadowCrystal={ShadowCrystal}
+            Chalcedony={Chalcedony}
+            DarkJade={DarkJade}
+            MonarchTopaz={MonarchTopaz}
+            ForestEmerald={ForestEmerald}
+            SkySapphire={SkySapphire}
+            AutumnsGlow={AutumnsGlow}
+            ScarletRuby={ScarletRuby}
+            TwilightOpal={TwilightOpal}
           />
         )}
       </div>
@@ -96,7 +180,7 @@ function EnchPrices({ handleIdust, handleGreatercosmic, handleDreamshard }) {
   );
 }
 
-function MatsPrices({ handleEternalEarth, handleSmallGem }) {
+function MatsPrices({ handleEternalEarth, handleSmallGem, buttonhandler }) {
   return (
     <>
       <form>
@@ -107,13 +191,17 @@ function MatsPrices({ handleEternalEarth, handleSmallGem }) {
           id="eternalearth"
           onChange={handleEternalEarth}
         />
-        <label htmlFor="smallgem">Small Gem Price - </label>
-        <input
-          type="number"
-          name="smallgem"
-          id="smallgem"
-          onChange={handleSmallGem}
-        />
+        {buttonhandler == 1 ? (
+          <>
+            <label htmlFor="smallgem">Small Ring Gem Price - </label>
+            <input
+              type="number"
+              name="smallgem"
+              id="smallgem"
+              onChange={handleSmallGem}
+            />
+          </>
+        ) : null}
       </form>
     </>
   );
@@ -145,14 +233,253 @@ function Bigitem({ idust, greatercosmic, dreamshard, eternalEarth }) {
   const result =
     idust * 5.5 * 0.74 + greatercosmic * 1.5 * 0.23 + dreamshard * 0.3;
   const ahTax = result * 0.05;
-  console.log(matsprice);
-  console.log(result);
-  console.log(ahTax);
   return (
     <>
       <div className="bigitemprofit">
         <p>Profit = {(result - matsprice).toFixed(2)} g</p>
         <p>Profit (AH tax 5%) = {(result - ahTax - matsprice).toFixed(2)} g</p>
+      </div>
+    </>
+  );
+}
+
+function Prospecting({
+  handleCobalt,
+  handleSaronite,
+  handleShadowCrystal,
+  handleChalcedony,
+  handleDarkJade,
+  handleMonarchTopaz,
+  handleForestEmerald,
+  handleSkySapphire,
+  handleAutumnsGlow,
+  handleScarletRuby,
+  handleTwilightOpal,
+  handleSmallGem,
+}) {
+  return (
+    <>
+      <form className="prospectingForm">
+        <label htmlFor="Cobalt">
+          Cobalt Price -{" "}
+          <input
+            type="number"
+            name="Cobalt"
+            id="Cobalt"
+            onChange={handleCobalt}
+          />
+        </label>
+        <label htmlFor="Saronite">
+          Saronite Ore Price -{" "}
+          <input
+            type="number"
+            name="Saronite"
+            id="Saronite"
+            onChange={handleSaronite}
+          />
+        </label>
+        <label htmlFor="smallgem">
+          Small Red/Yellow/Orange Price -{" "}
+          <input
+            type="number"
+            name="smallgem"
+            id="smallgem"
+            onChange={handleSmallGem}
+          />
+        </label>
+        <label htmlFor="ShadowCrystal">
+          Shadow Crystal Price -{" "}
+          <input
+            type="number"
+            name="ShadowCrystal"
+            id="ShadowCrystal"
+            onChange={handleShadowCrystal}
+          />
+        </label>
+        <label htmlFor="Chalcedony">
+          Chalcedony Price -{" "}
+          <input
+            type="number"
+            name="Chalcedony"
+            id="Chalcedony"
+            onChange={handleChalcedony}
+          />
+        </label>
+        <label htmlFor="DarkJade">
+          Dark Jade Price -{" "}
+          <input
+            type="number"
+            name="DarkJade"
+            id="DarkJade"
+            onChange={handleDarkJade}
+          />
+        </label>
+        <label htmlFor="MonarchTopaz">
+          Big Orange Price -{" "}
+          <input
+            type="number"
+            name="MonarchTopaz"
+            id="MonarchTopaz"
+            onChange={handleMonarchTopaz}
+          />
+        </label>
+        <label htmlFor="ForestEmerald">
+          Big Green Price -{" "}
+          <input
+            type="number"
+            name="ForestEmerald"
+            id="ForestEmerald"
+            onChange={handleForestEmerald}
+          />
+        </label>
+        <label htmlFor="SkySapphire">
+          Big Blue Price -{" "}
+          <input
+            type="number"
+            name="SkySapphire"
+            id="SkySapphire"
+            onChange={handleSkySapphire}
+          />
+        </label>
+        <label htmlFor="AutumnsGlow">
+          Big Yellow Price -{" "}
+          <input
+            type="number"
+            name="AutumnsGlow"
+            id="AutumnsGlow"
+            onChange={handleAutumnsGlow}
+          />
+        </label>
+        <label htmlFor="ScarletRuby">
+          Big Red Price -{" "}
+          <input
+            type="number"
+            name="ScarletRuby"
+            id="ScarletRuby"
+            onChange={handleScarletRuby}
+          />
+        </label>
+        <label htmlFor="TwilightOpal">
+          Big Purple Price -{" "}
+          <input
+            type="number"
+            name="TwilightOpal"
+            id="TwilightOpal"
+            onChange={handleTwilightOpal}
+          />
+        </label>
+      </form>
+    </>
+  );
+}
+
+// big gem 12h x1 = 45s
+// small gem 12h 1x = 3.75s
+function ProspectingResult({
+  Cobalt,
+  Saronite,
+  smallGem,
+  ShadowCrystal,
+  Chalcedony,
+  DarkJade,
+  MonarchTopaz,
+  ForestEmerald,
+  SkySapphire,
+  AutumnsGlow,
+  ScarletRuby,
+  TwilightOpal,
+}) {
+  const matspriceCobalt = Cobalt * 5;
+  const resultCobalt =
+    ShadowCrystal * 0.23 +
+    Chalcedony * 0.23 +
+    DarkJade * 0.23 +
+    MonarchTopaz * 0.013 +
+    ForestEmerald * 0.013 +
+    SkySapphire * 0.013 +
+    AutumnsGlow * 0.013 +
+    ScarletRuby * 0.013 +
+    TwilightOpal * 0.013 +
+    smallGem * 3 * 0.23;
+
+  const ahTaxCobalt = resultCobalt * 0.05;
+  const PostTaxCobalt =
+    ShadowCrystal +
+    0.0375 * 0.23 +
+    Chalcedony +
+    0.0375 * 0.23 +
+    DarkJade +
+    0.0375 * 0.23 +
+    MonarchTopaz +
+    0.45 * 0.013 +
+    ForestEmerald +
+    0.45 * 0.013 +
+    SkySapphire +
+    0.45 * 0.013 +
+    AutumnsGlow +
+    0.45 * 0.013 +
+    ScarletRuby +
+    0.45 * 0.013 +
+    TwilightOpal +
+    0.45 * 0.013 +
+    smallGem +
+    0.0375 * 3 * 0.23;
+  const matspriceSaronite = Saronite * 5;
+  const resultSaronite =
+    ShadowCrystal * 0.18 +
+    Chalcedony * 0.18 +
+    DarkJade * 0.18 +
+    MonarchTopaz * 0.04 +
+    ForestEmerald * 0.04 +
+    SkySapphire * 0.04 +
+    AutumnsGlow * 0.04 +
+    ScarletRuby * 0.04 +
+    TwilightOpal * 0.04 +
+    smallGem * 3 * 0.18;
+
+  const ahTaxSaronite = resultSaronite * 0.05;
+  const PostTaxSaronite =
+    ShadowCrystal +
+    0.0375 * 0.18 +
+    Chalcedony +
+    0.0375 * 0.18 +
+    DarkJade +
+    0.0375 * 0.18 +
+    MonarchTopaz +
+    0.45 * 0.04 +
+    ForestEmerald +
+    0.45 * 0.04 +
+    SkySapphire +
+    0.45 * 0.04 +
+    AutumnsGlow +
+    0.45 * 0.04 +
+    ScarletRuby +
+    0.45 * 0.04 +
+    TwilightOpal +
+    0.45 * 0.04 +
+    smallGem +
+    0.0375 * 3 * 0.18;
+
+  return (
+    <>
+      <div className="CobaltProfit">
+        <p>Cobalt Profit = {(resultCobalt - matspriceCobalt).toFixed(2)} g</p>
+        <p>
+          Cobalt Profit (AH tax 5% + 12h auction) ={" "}
+          {resultCobalt == 0 ? 0 : (PostTaxCobalt - ahTaxCobalt).toFixed(2)} g
+        </p>
+      </div>
+      <div className="SaroniteProfit">
+        <p>
+          Saronite Profit = {(resultSaronite - matspriceSaronite).toFixed(2)} g
+        </p>
+        <p>
+          Saronite Ore Profit (AH tax 5% + 12h auction) ={" "}
+          {resultSaronite == 0
+            ? 0
+            : (PostTaxSaronite - ahTaxSaronite).toFixed(2)}{" "}
+          g
+        </p>
       </div>
     </>
   );
